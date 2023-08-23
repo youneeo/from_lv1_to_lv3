7103e0f896259f32095619c5b069a34fa67549fa
youneeo <steamdacut@gmail.com>
2023-08-23 16:04:45 +0900

    [feature] string./feature update.

--- app/src/main/java/com/android/app_homeworked_start/HomeActivity.kt
+++ app/src/main/java/com/android/app_homeworked_start/HomeActivity.kt
@@ -1,16 +1,23 @@
 package com.android.app_homeworked_start
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val btn = findViewById<Button>(R.id.btn_finish)
-        btn.setOnClickListener {
+
+        val home_id = findViewById<TextView>(R.id.home_id)
+
+        if (intent.hasExtra("id")){
+        home_id.text ="아이디: "+ intent.getStringExtra("id")
+        }
+
+        val btn_closed = findViewById<Button>(R.id.btn_finish)
+        btn_closed.setOnClickListener {
             finish()
         }
     }
--- app/src/main/java/com/android/app_homeworked_start/SignUpActivity.kt
+++ app/src/main/java/com/android/app_homeworked_start/SignUpActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.android.app_homeworked_start.R.id.btn_sign_up_2
 
 class SignUpActivity : AppCompatActivity() {
@@ -12,8 +14,18 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+
+        val et_name = findViewById<EditText>(R.id.et_name)
+        val et_id = findViewById<EditText>(R.id.et_id_sign_up)
+        val et_psw = findViewById<EditText>(R.id.et_psw_sign_up)
         val btn = findViewById<Button>(btn_sign_up_2)
+        
         btn.setOnClickListener {
+            if(et_name.toString().trim().isEmpty() or et_id.toString().trim().isEmpty() or et_psw.toString().trim().isEmpty()){
+                Toast.makeText(this, "입력되지않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
